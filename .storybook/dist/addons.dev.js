abfa6d74a5046f32f53d7ab22d4b3cff00f93ddf
Taoyu Chen <875693150@qq.com>
2020-11-12 22:06:25 +0800

    Fix upcoming movies page

---
+++ .storybook/dist/addons.dev.js
@@ -0,0 +1,5 @@
+"use strict";
+
+module.exports = {
+  addons: ['@storybook/addon-actions/register']
+};
--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-import StubAPI from "../api/stubAPI";
+//import StubAPI from "../api/stubAPI";
 import PageTemplate from '../components/templateMovieListPage'
 import { getUpcomingMovies } from "../api/tmdb-api";
+import AddToFavoritesButton from '../components/buttons/addToFavorites';
 
 const UpcomingMoviesPage = () => {
   const [movies, setMovies] = useState([]);
@@ -11,22 +12,15 @@ const UpcomingMoviesPage = () => {
     });
   }, []);
 
-  const addToFavorites = movieId => {
-    setMovies(movies => {
-      const index = movies.map(m => m.id).indexOf(movieId);
-      StubAPI.add(movies[index]);
-      let newMoviesState = [...movies]
-      newMoviesState.splice(index, 1);
-      return newMoviesState;
-    });
-  };
 
   return (
       <PageTemplate
         title='Upcoming Movies'
         movies={movies}
-        buttonHandler={addToFavorites}
-      />
+        action={(movie) => {
+          return <AddToFavoritesButton movie={movie} />
+      }}
+    />
   );
 };
 
